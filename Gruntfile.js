96ccad7edd151ae316893a191fd85f38360a60f4
Hack Reactor Students <accounts@hackreactor.com>
2016-08-24 16:26:47 -0700

    Add nodemon task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  //grrrrunting
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
@@ -71,10 +71,13 @@ module.exports = function(grunt) {
   ////////////////////////////////////////////////////
   // Main grunt tasks
   ////////////////////////////////////////////////////
+  // grunt.registerTask('nodemon', ['nodemon']);
+
 
   grunt.registerTask('test', [
     'mochaTest'
   ]);
+
 
   grunt.registerTask('build', [
   ]);
